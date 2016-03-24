0d4c0a2782eb6ebbcddaa1a9726e92655c9f0f34
Ed Lewis <edward.richard.lewis@gmail.com>
2016-03-24 16:42:08 +0000

    bump version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -21,7 +21,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization  := "ed",
-    version       := "0.1.4",
+    version       := "0.1.5",
     description   := "Idiosyncratic, small general-purpose tools for Scala",
     scalaVersion  := "2.9.1",
     scalacOptions := Seq("-deprecation", "-encoding", "utf8")
