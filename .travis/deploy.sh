8555f4a18ca44d8a609eaed96b59c1c08aa9f45d
Ed Lewis <edward.richard.lewis@gmail.com>
2016-03-24 16:09:37 +0000

    bash..

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-function make_creds {
-    mkdir ~/.bintray/
-    FILE=$HOME/.bintray/.credentials
-    cat <<EOF >$FILE
-    realm = Bintray API Realm
-    host = api.bintray.com
-    user = $BINTRAY_USER
-    password = $BINTRAY_API_KEY
-    EOF
-}
+mkdir ~/.bintray/
+FILE=$HOME/.bintray/.credentials
+cat <<EOF >$FILE
+realm = Bintray API Realm
+host = api.bintray.com
+user = $BINTRAY_USER
+password = $BINTRAY_API_KEY
+EOF
 
-make_creds
 cd $TRAVIS_BUILD_DIR
+pwd
 sbt release
 
